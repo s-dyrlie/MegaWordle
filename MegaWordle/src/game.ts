e73f06f34c67fd0dfdc2900fb4228ab64eba9638
selmadyrlie <105597832+s-dyrlie@users.noreply.github.com>
2026-08-18 10:23:49 +0200

    initial commit: project structure and evaluateGuess logic

---
+++ MegaWordle/src/game.ts
@@ -0,0 +1,37 @@
+export type tileStatus = "green" | "yellow" | "gray";
+
+export const word_length = 8;
+export const max_attempts = 6;
+
+export function evaluateGuess(guess: string, answer: string): tileStatus[] {
+    const g = guess.toUpperCase().split("");
+    const a = answer.toUpperCase().split("");
+    const result: tileStatus[] = new Array(g.length).fill("gray");
+
+    const remaining: Record<string, number> = {};
+        for (const letter of a) {
+            remaining[letter] = (remaining[letter] ?? 0) + 1;
+        } 
+
+        // find green letters
+        for (let i = 0; i < g.length; i++) {
+            if (g[i] === a[i]) {
+                result[i] = "green";
+                remaining[g[i]] -= 1;
+            }
+        }
+
+        //find yellow letters
+        for (let i = 0; i < g.length; i++) {
+            //if the letter is already marked as green, continue without further checking
+            if (result[i] === "green") continue;
+
+            if (remaining[g[i]] > 0) {
+                result[i] = "yellow";
+                remaining[g[i]] -= 1; //use another one of that letter
+            }
+
+                //if no match, the letter keeps it's value as "gray"
+        }
+    return result;
+}
---
+++ MegaWordle/src/test.ts
@@ -0,0 +1,10 @@
+import { evaluateGuess } from "./game";
+
+printResult("painterS", "MOUntain")
+
+
+function printResult(guess: string, answer: string) {
+    const result = evaluateGuess(guess, answer);
+    console.log("you guessed: " + guess.toUpperCase());
+    console.log("result: " + result.join(" | "));
+
